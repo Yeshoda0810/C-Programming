37aa113e000d2a2d1e6f3031b74a8de7044cd776
Yeshoda0810 <yashoda83741@gmail.com>
2024-10-21 12:15:04 +0530

    Create voteeligibility.c

---
+++ voteeligibility.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("eligible to vote");
+    }
+    else
+    {
+        printf("not eligible to vote");
+    }
+}
