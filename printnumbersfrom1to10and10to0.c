dfe5e20d4922a8444ac59d8d9f68287cedf73352
Yeshoda0810 <yashoda83741@gmail.com>
2024-12-30 12:29:49 +0530

    Create printnumbersfrom1to10and10to0.c

---
+++ printnumbersfrom1to10and10to0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i,n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    for(i=0;i<=10;++i)
+    {
+        printf("%d\n",i);
+    }
+    for(i=10;i>=0;--i)
+    {
+        printf("%d\n",i);
+    }
+}
