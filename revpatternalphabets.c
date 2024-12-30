86806c4fc7cdecf88b294c15f6a987ddb4a79ca4
Yeshoda0810 <yashoda83741@gmail.com>
2024-12-30 11:48:45 +0530

    Create revpatternalphabets.c

---
+++ revpatternalphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int i,j;
+    char n='A';
+    for(i=1;i<=5;++i)
+    {
+        for(j=5;j>=i;--j)
+        {
+            printf("%c\t",n);
+        }
+        n++;
+        printf("\n");
+    }
+}
